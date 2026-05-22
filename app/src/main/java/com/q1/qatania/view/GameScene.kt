6f6cc2646d429720c6e2723b0db39bb0acec84e3
tobi.wurzer <tobias@q1studios.at>
2026-05-22 16:20:22 +0200

    removed unused composable

--- app/src/main/java/com/q1/qatania/view/GameScene.kt
+++ app/src/main/java/com/q1/qatania/view/GameScene.kt
@@ -2,26 +2,17 @@ package com.q1.qatania.view
 
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.android.filament.LightManager
 import com.q1.qatania.dataRepository.GameBoardRepository
-import com.q1.qatania.model.Tile
 import com.q1.qatania.viewmodel.GameBoardViewModel
 import com.q1.qatania.viewmodel.GameBoardViewModelFactory
 import dev.romainguy.kotlin.math.Float3
 import io.github.sceneview.SceneView
-import io.github.sceneview.createDefaultCameraManipulator
-import io.github.sceneview.loaders.ModelLoader
-import io.github.sceneview.math.Position
-import io.github.sceneview.node.ModelNode
 import io.github.sceneview.rememberCameraManipulator
 import io.github.sceneview.rememberCameraNode
 import io.github.sceneview.rememberEngine
@@ -60,27 +51,23 @@ fun GameScene() {
 
     ) {
 
-
+        // Spawn Tiles
         tiles?.forEach { tile ->
-            /*
-            TileNode(tile = tile, modelLoader = modelLoader)
-            */
             rememberModelInstance(modelLoader, tile.type.path)?.let {
-                ModelNode(modelInstance = it,
+                ModelNode(
+                    modelInstance = it,
                     scaleToUnits = 1.0f,
                     autoAnimate = true, position = Float3(
-                    tile.coordinates[0].toFloat(),
-                    0f,
-                    tile.coordinates[1].toFloat()
-                )
+                        tile.coordinates[0].toFloat(),
+                        0f,
+                        tile.coordinates[1].toFloat()
+                    )
                 )
             }
-
             android.util.Log.d(
                 "GameScene",
                 "Spawning TileNode for tile: ${tile.id}, ${tile.type}, ${tile.coordinates}"
             )
-
         }
 
 
@@ -93,17 +80,3 @@ fun GameScene() {
         )*/
     }
 }
-
-
-@Composable
-fun TileNode(tile: Tile, modelLoader: ModelLoader) {
-    rememberModelInstance(modelLoader, tile.type.path)?.let {
-        ModelNode(modelInstance = it, scaleToUnits = 0.5f, autoAnimate = true)
-    }
-    android.util.Log.d(
-        "GameScene",
-        "Spawning TileNode for tile: ${tile.id}, path should be: ${tile.type.path}"
-    )
-}
-
-
